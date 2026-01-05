f99ef3a3e538c0762d24ce99133537a71868c89b
hej090224 <fc49854985@gmail.com>
2026-01-05 15:11:23 +0900

    init

---
+++ src/main/java/com/example/joycenterserver/JoyCenterServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.joycenterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyCenterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoyCenterServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/joycenterserver/JoyCenterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joycenterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyCenterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
