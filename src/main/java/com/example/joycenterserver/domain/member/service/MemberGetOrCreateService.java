ba0654cf91b4430f12157abc434d8dcee12b6c9c
hej090224 <fc49854985@gmail.com>
2026-01-06 20:10:50 +0900

    feat: 회원 조회 및 생성 서비스 추가

---
+++ src/main/java/com/example/joycenterserver/domain/member/service/MemberGetOrCreateService.java
@@ -0,0 +1,8 @@
+package com.example.joycenterserver.domain.member.service;
+
+import com.example.joycenterserver.domain.member.entity.Member;
+
+public interface MemberGetOrCreateService {
+
+    Member getOrCreate(String email);
+}
