a6516bdf1a630f942b3126cdf1c4d239a2265f7d
hej090224 <fc49854985@gmail.com>
2026-01-12 23:17:28 +0900

    feat: 공통 에러 응답을 위한 ErrorResponse DTO 추가

---
+++ src/main/java/com/example/joycenterserver/global/error/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.example.joycenterserver.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private int status;
+    private String message;
+}
