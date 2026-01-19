09c66a7f23cbc57dd5f20f783cbc3635ba9f028b
hej090224 <fc49854985@gmail.com>
2026-01-20 08:11:09 +0900

    feat: 게시글 조회 시 블록 조회 로직 추가

--- src/main/java/com/example/joycenterserver/domain/post/service/impl/PostGetServiceImpl.java
+++ src/main/java/com/example/joycenterserver/domain/post/service/impl/PostGetServiceImpl.java
@@ -1,13 +1,17 @@
 package com.example.joycenterserver.domain.post.service.impl;
 
+import com.example.joycenterserver.domain.post.dto.response.PostBlockResponse;
 import com.example.joycenterserver.domain.post.dto.response.PostDetailResponse;
 import com.example.joycenterserver.domain.post.entity.Post;
 import com.example.joycenterserver.domain.post.exception.NotFoundPostException;
+import com.example.joycenterserver.domain.post.repository.PostBlockRepository;
 import com.example.joycenterserver.domain.post.repository.PostRepository;
 import com.example.joycenterserver.domain.post.service.PostGetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,12 +19,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostGetServiceImpl implements PostGetService {
 
     private final PostRepository postRepository;
+    private final PostBlockRepository postBlockRepository;
 
     @Override
     public PostDetailResponse getById(Long postId) {
         Post post = postRepository.findById(postId)
                 .orElseThrow(NotFoundPostException::new);
 
-        return PostDetailResponse.from(post);
+        List<PostBlockResponse> blocks = postBlockRepository.findAllByPostOrderByBlockOrderAsc(post)
+                .stream()
+                .map(PostBlockResponse::from)
+                .toList();
+
+        return PostDetailResponse.from(post, blocks);
     }
 }
