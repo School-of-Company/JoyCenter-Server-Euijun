f641cbdcacea34ae6b447b8565c62dc413c7da1a
hej090224 <fc49854985@gmail.com>
2026-01-29 15:43:40 +0900

    feat: 게시글 목록 조회 시 정렬 타입 기반 페이징 처리 구현

--- src/main/java/com/example/joycenterserver/domain/post/service/PostListService.java
+++ src/main/java/com/example/joycenterserver/domain/post/service/PostListService.java
@@ -1,8 +1,8 @@
 package com.example.joycenterserver.domain.post.service;
 
 import com.example.joycenterserver.domain.post.dto.response.PostListResponse;
+import com.example.joycenterserver.domain.post.entity.PostSortType;
 
 public interface PostListService {
-
-    PostListResponse getList(int page, int size, String sort);
+    PostListResponse getList(int page, int size, PostSortType sortType);
 }
