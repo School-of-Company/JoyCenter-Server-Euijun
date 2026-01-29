a4d0640fe67cf96069a04f2fd43999f487838944
hej090224 <fc49854985@gmail.com>
2026-01-29 15:43:53 +0900

    refactor: 게시글 조회 서비스 구현체 구조 정리

--- src/main/java/com/example/joycenterserver/domain/post/service/PostGetService.java
+++ src/main/java/com/example/joycenterserver/domain/post/service/PostGetService.java
@@ -1,8 +1,11 @@
 package com.example.joycenterserver.domain.post.service;
 
 import com.example.joycenterserver.domain.post.dto.response.PostDetailResponse;
+import com.example.joycenterserver.domain.post.dto.response.PostListResponse;
+import com.example.joycenterserver.domain.post.entity.PostSortType;
 
 public interface PostGetService {
 
     PostDetailResponse getById(Long postId);
+    PostListResponse getList(PostSortType sortType);
 }
--- src/main/java/com/example/joycenterserver/domain/post/service/impl/PostGetServiceImpl.java
+++ src/main/java/com/example/joycenterserver/domain/post/service/impl/PostGetServiceImpl.java
@@ -2,12 +2,16 @@ package com.example.joycenterserver.domain.post.service.impl;
 
 import com.example.joycenterserver.domain.post.dto.response.PostBlockResponse;
 import com.example.joycenterserver.domain.post.dto.response.PostDetailResponse;
+import com.example.joycenterserver.domain.post.dto.response.PostListResponse;
 import com.example.joycenterserver.domain.post.entity.Post;
+import com.example.joycenterserver.domain.post.entity.PostSortType;
 import com.example.joycenterserver.domain.post.exception.NotFoundPostException;
 import com.example.joycenterserver.domain.post.repository.PostBlockRepository;
 import com.example.joycenterserver.domain.post.repository.PostRepository;
 import com.example.joycenterserver.domain.post.service.PostGetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,11 +30,21 @@ public class PostGetServiceImpl implements PostGetService {
         Post post = postRepository.findById(postId)
                 .orElseThrow(NotFoundPostException::new);
 
-        List<PostBlockResponse> blocks = postBlockRepository.findAllByPostOrderByBlockOrderAsc(post)
-                .stream()
-                .map(PostBlockResponse::from)
-                .toList();
+        List<PostBlockResponse> blocks =
+                postBlockRepository.findAllByPostOrderByBlockOrderAsc(post)
+                        .stream()
+                        .map(PostBlockResponse::from)
+                        .toList();
 
         return PostDetailResponse.from(post, blocks);
+    }
+
+    @Override
+    public PostListResponse getList(PostSortType sortType) {
+        Pageable pageable = PageRequest.of(0, 20, sortType.toSort());
+
+        return PostListResponse.from(
+                postRepository.findAll(pageable)
+        );
     }
 }
